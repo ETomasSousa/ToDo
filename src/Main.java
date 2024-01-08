ea736a440fe582f88e6efb7ad7343a5b420794dd
Skyrush <tomasms03@gmail.com>
2024-01-08 15:11:05 +0000

    [Model] Initial model

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception{
+        if(args.length != 2)
+            throw new Exception("Invalid parameters");
+        
+    }
+}
