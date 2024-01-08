1a27575820385180116cef79fab9821fb05c40e6
Skyrush <tomasms03@gmail.com>
2024-01-08 15:15:29 +0000

    [Addition] New classes {IOReader/Writer, Console and Menu}

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import io.IOReader;
+import manager.TaskManager;
+
 public class Main {
     public static void main(String[] args) throws Exception{
         if(args.length != 2)
             throw new Exception("Invalid parameters");
-        
+
+        IOReader ioreader = new IOReader(args[1]);
+        TaskManager taskManager = new TaskManager(IOReader);
+
+        taskManager.start();
     }
 }
