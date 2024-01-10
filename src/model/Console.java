f54215ea7ccf66bfb7be1e0e85d3b52cb9f3528b
Skyrush <tomasms03@gmail.com>
2024-01-10 20:03:07 +0000

    [UI] UI implementation

--- src/model/Console.java
+++ src/model/Console.java
@@ -1,5 +1,92 @@
 package model;
 
+import java.util.List;
+
+import manager.TaskManager;
+
 public class Console {
     
+    private Menu menu;
+    private boolean quit;
+    private TaskManager taskManager;
+    
+    /* Constructor vazio */
+    public Console() {
+
+        this.quit = false;
+
+        this.menu = new Menu();
+        this.menu.addOption("CREATE", 0);
+        this.menu.addOption("OPEN", 1);
+        this.menu.addOption("EDIT", 1);
+        this.menu.addOption("REMOVE", 1);
+        this.menu.addOption("LIST", 1);
+        this.menu.addOption("HELP", 0);
+        this.menu.addOption("QUIT", 0);
+    }
+
+    /* Corre a consola de comandos */
+    public void run() {
+        /* Corre a consola enquanto houver ligação */
+        while (!this.quit) {
+            this.menu.readCommand();
+            this.readArguments(this.menu.getArguments());   
+        }
+    }
+
+    /* Lê os argumentos obtidos na leitura de comandos */
+    private void readArguments(List<String> args) {
+
+        /* Procura pelo comando nos argumentos */
+        String cmd = args.get(0);
+
+        /* Handler do comando "HELP" */
+        if (cmd.equals("HELP")) {
+            this.menu.display();
+        }
+        
+        /* Handler do comando "QUIT" */
+        if (cmd.equals("QUIT")) {
+            this.quit = true;
+            System.out.println("Exiting...");
+            System.exit(1);
+        }
+
+        /* Handler do comando "CREATE" */
+        if (cmd.equals("CREATE")) {
+            this.taskManager.createTask();
+        }
+
+        if (cmd.equals("EDIT")) {
+            if (!this.menu.checkNumberArgs(1))
+                System.out.println("Invalid arguments! Type HELP to get more information...");
+            else {
+                this.taskManager.editTask(args.get(1));
+            }
+        }
+
+        if (cmd.equals("REMOVE")) {
+            if (!this.menu.checkNumberArgs(1))
+                System.out.println("Invalid arguments! Type HELP to get more information...");
+            else {
+                this.taskManager.removeTask(args.get(1));
+            }
+        }
+
+        if (cmd.equals("OPEN")) {
+            if (!this.menu.checkNumberArgs(1))
+                System.out.println("Invalid arguments! Type HELP to get more information...");
+                else {
+                    this.taskManager.openTask(args.get(1));
+                }
+        }
+
+        if (cmd.equals("LIST")) {
+            if (!this.menu.checkNumberArgs(0))
+                System.out.println("Invalid arguments! Type HELP to get more information...");
+                else {
+                    this.taskManager.listTasks();
+                }
+        }
+    }
 }
--- src/model/Menu.java
+++ src/model/Menu.java
@@ -1,5 +1,83 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Menu {
     
+    private Map<String,Option> commands;
+    private List<String> arguments;
+
+    /* Constructors */
+    public Menu() {
+        this.commands = new HashMap<>();
+
+        Option cmd = new Option();
+        this.commands.put(cmd.getCommand(), cmd);
+    }
+
+    /* Getters */
+    public List<String> getArguments() {
+        return new ArrayList<>(this.arguments);
+    }
+
+    /* Setters */
+    public void addOption(String command, int arguments) {
+        this.commands.put(command, new Option(command, arguments));
+    }
+
+    /* Auxiliar */
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(Menu)commands:").append(this.commands).append(";");
+        builder.append("arguments:").append(this.arguments).append(";");
+
+        return builder.toString();
+    }
+
+    public void display() {
+
+        System.out.println("*** HELP ***");
+        
+        for (Option command : this.commands.values())
+            System.out.println(command.display());
+
+        System.out.println("************");
+    }
+
+    public boolean checkNumberArgs(int num) {
+        if (num == this.arguments.size() - 1)
+            return true;    
+        return false;
+    }
+
+    public void readCommand() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String argument = scanner.nextLine();
+
+        List <String> args = Arrays.asList(argument.split(" "));
+
+        if (!this.commands.containsKey(args.get(0))) {
+            System.out.println("Invalid command! Type HELP to get more information...");
+            this.readCommand();
+            return;
+        }
+
+        if (!this.commands.get(args.get(0)).validCommand(args)) {
+            System.out.println("Invalid arguments! Type HELP to get more information...");
+            this.readCommand();
+            return;
+        }
+
+        this.arguments = args;
+        
+    }
 }
